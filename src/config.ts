059d69c4e548d2c3595c0287589194c333f1af31
Daniel Chae <daniel.jiwoong@gmail.com>
2025-08-09 17:07:27 +0900

    chore: using styling/components from starlight-theme-black
    - instead of applying it as a theme

---
+++ src/config.ts
@@ -0,0 +1,11 @@
+export const userConfig = {
+  navLinks: [
+    {
+      label: "Case Study",
+      link: "/case-study/intro",
+      attrs: {},
+    },
+  ],
+  footerText:
+    "Kubrick 2025. Source code available on [Github](https://github.com/kubrick-ai/kubrick-site).",
+};
