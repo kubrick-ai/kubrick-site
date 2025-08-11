fa56381cc9b753b9a3cbdfb1068126adfa213cc6
Daniel Chae <daniel.jiwoong@gmail.com>
2025-08-12 06:49:50 +0900

    chore: clean up astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,29 +12,60 @@ export default defineConfig({
 
   integrations: [
     starlight({
-      plugins: [
-        starlightOpenAPI([
-          { base: "api", schema: "./src/schemas/api-schema.yaml" },
-        ]),
-      ],
-      components: {
-        Hero: "./src/components/Hero.astro",
-        Header: "./src/components/Header.astro",
-        Footer: "./src/components/Footer.astro",
-        Sidebar: "./src/components/Sidebar.astro",
-        PageFrame: "./src/components/PageFrame.astro",
-        ThemeSelect: "./src/components/ThemeSelect.astro",
-        SiteTitle: "./src/components/SiteTitle.astro",
-        Search: "./src/components/Search.astro",
-        SocialIcons: "./src/components/SocialIcons.astro",
-        PageTitle: "./src/components/PageTitle.astro",
-        MarkdownContent: "./src/components/MarkdownContent.astro",
-        ContentPanel: "./src/components/ContentPanel.astro",
-        TwoColumnContent: "./src/components/TwoColumnContent.astro",
-        Pagination: "./src/components/Pagination.astro",
-        TableOfContents: "./src/components/TableOfContents.astro",
-        PageSidebar: "./src/components/PageSidebar.astro",
+      title: "Kubrick",
+      favicon: "/favicon.ico",
+      logo: {
+        src: "./src/assets/logo.svg",
       },
+      // Edit Navbar configuration in ./src/config.ts
+
+      social: [
+        {
+          icon: "github",
+          label: "GitHub",
+          href: "https://github.com/kubrick-ai",
+        },
+      ],
+
+      sidebar: [
+        {
+          label: "Case Study",
+          autogenerate: { directory: "case-study" },
+        },
+        {
+          label: "Guides",
+          autogenerate: { directory: "guides" },
+        },
+        {
+          label: "Reference",
+          autogenerate: { directory: "api" },
+        },
+        ...openAPISidebarGroups,
+      ],
+
+      editLink: {
+        baseUrl: "https://github.com/kubrick-ai/kubrick-site/edit/main/docs",
+      },
+
+      expressiveCode: {
+        themes: ["github-dark-default", "github-light-default"],
+        styleOverrides: {
+          codeBackground: "var(--code-background)",
+          borderWidth: "0px",
+          borderRadius: "calc(var(--radius) + 4px)",
+          gutterBorderWidth: "0px",
+          frames: {
+            copyIcon: createInlineSvgUrl(
+              `<svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-clipboard"><rect width="8" height="4" x="8" y="2" rx="1" ry="1"></rect><path d="M16 4h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h2"></path></svg>`
+            ),
+          },
+          textMarkers: {
+            markBackground: "var(--mark-background)",
+            markBorderColor: "var(--border)",
+          },
+        },
+      },
+
       customCss: [
         "@fontsource/geist-mono/100.css",
         "@fontsource/geist-mono/200.css",
@@ -59,55 +90,30 @@ export default defineConfig({
         "./src/styles/base.css",
       ],
 
-      title: "Kubrick",
-      favicon: "/favicon.ico",
-      logo: {
-        src: "./src/assets/logo.svg",
+      components: {
+        Hero: "./src/components/Hero.astro",
+        Header: "./src/components/Header.astro",
+        Footer: "./src/components/Footer.astro",
+        Sidebar: "./src/components/Sidebar.astro",
+        PageFrame: "./src/components/PageFrame.astro",
+        ThemeSelect: "./src/components/ThemeSelect.astro",
+        SiteTitle: "./src/components/SiteTitle.astro",
+        Search: "./src/components/Search.astro",
+        SocialIcons: "./src/components/SocialIcons.astro",
+        PageTitle: "./src/components/PageTitle.astro",
+        MarkdownContent: "./src/components/MarkdownContent.astro",
+        ContentPanel: "./src/components/ContentPanel.astro",
+        TwoColumnContent: "./src/components/TwoColumnContent.astro",
+        Pagination: "./src/components/Pagination.astro",
+        TableOfContents: "./src/components/TableOfContents.astro",
+        PageSidebar: "./src/components/PageSidebar.astro",
       },
 
-      social: [
-        {
-          icon: "github",
-          label: "GitHub",
-          href: "https://github.com/kubrick-ai",
-        },
+      plugins: [
+        starlightOpenAPI([
+          { base: "api", schema: "./src/schemas/api-schema.yaml" },
+        ]),
       ],
-      sidebar: [
-        {
-          label: "Case Study",
-          autogenerate: { directory: "case-study" },
-        },
-        {
-          label: "Guides",
-          autogenerate: { directory: "guides" },
-        },
-        {
-          label: "Reference",
-          autogenerate: { directory: "api" },
-        },
-        ...openAPISidebarGroups,
-      ],
-      editLink: {
-        baseUrl: "https://github.com/kubrick-ai/kubrick-site/edit/main/docs",
-      },
-      expressiveCode: {
-        themes: ["github-dark-default", "github-light-default"],
-        styleOverrides: {
-          codeBackground: "var(--code-background)",
-          borderWidth: "0px",
-          borderRadius: "calc(var(--radius) + 4px)",
-          gutterBorderWidth: "0px",
-          frames: {
-            copyIcon: createInlineSvgUrl(
-              `<svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-clipboard"><rect width="8" height="4" x="8" y="2" rx="1" ry="1"></rect><path d="M16 4h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h2"></path></svg>`
-            ),
-          },
-          textMarkers: {
-            markBackground: "var(--mark-background)",
-            markBorderColor: "var(--border)",
-          },
-        },
-      },
     }),
   ],
 
