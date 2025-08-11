64819d2a52745470251ad5566e0a736158ec1d6d
Daniel Chae <daniel.jiwoong@gmail.com>
2025-08-11 14:07:16 +0900

    feat: Our Team page + layer fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -49,7 +49,6 @@ export default defineConfig({
         "@fontsource/geist-sans/800.css",
         "@fontsource/geist-sans/900.css",
         "./src/styles/global.css",
-        "./src/styles/layers.css",
         "./src/styles/theme.css",
         "./src/styles/base.css",
       ],
@@ -93,7 +92,7 @@ export default defineConfig({
           gutterBorderWidth: "0px",
           frames: {
             copyIcon: createInlineSvgUrl(
-              `<svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-clipboard"><rect width="8" height="4" x="8" y="2" rx="1" ry="1"></rect><path d="M16 4h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h2"></path></svg>`,
+              `<svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-clipboard"><rect width="8" height="4" x="8" y="2" rx="1" ry="1"></rect><path d="M16 4h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h2"></path></svg>`
             ),
           },
           textMarkers: {
--- src/config.ts
+++ src/config.ts
@@ -7,6 +7,16 @@ export const userConfig: StarlightThemeBlackUserConfig = {
       link: "/case-study/intro",
       attrs: {},
     },
+    {
+      label: "Docs",
+      link: "/guides/get-started",
+      attrs: {},
+    },
+    {
+      label: "Team",
+      link: "/team",
+      attrs: {},
+    },
   ],
   footerText:
     "Kubrick 2025. Source code available on [Github](https://github.com/kubrick-ai/kubrick-site).",
