1172cf72718f3af19e14187df03ab06274aedc6d
Daniel Chae <daniel.jiwoong@gmail.com>
2025-08-12 16:06:58 +0900

    deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   // site: "https://kubrick-ai.github.io/kubrick-site",
+
   site: "https://kubrick-ai.github.io/",
   base: "/kubrick-site",
 
