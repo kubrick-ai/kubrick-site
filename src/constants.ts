b8f4386181291641d2eb518fc00accac5d862a0d
Daniel Chae <daniel.jiwoong@gmail.com>
2025-08-21 08:29:06 +0900

    feat: moved all demo media to hosted

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,13 @@
-export const PAGE_TITLE_ID = '_top'
+export const PAGE_TITLE_ID = "_top";
+
+export const MEDIA_URLS = {
+  cliDeployment: "https://i.imgur.com/SfmlcpL.mp4",
+  cliDestroy: "https://i.imgur.com/tgt5HdH.mp4",
+  ingestionDemo: "https://i.imgur.com/1gXa1UK.mp4",
+  textQueryDemo: "https://i.imgur.com/iV6V75j.mp4",
+  imageQueryDemo: "https://i.imgur.com/WTXlBR1.mp4",
+  videoQueryDemo: "https://i.imgur.com/NqonMzx.mp4",
+  textQueryDemoLanding: "https://i.imgur.com/h1s9lZM.mp4",
+  imageQueryDemoLanding: "https://i.imgur.com/5w9Hr7h.mp4",
+  ingestionDemoLanding: "https://i.imgur.com/vUrQ7lU.mp4",
+};
